c9b69a0dfc8d2e3586690939e8eb339f69fad919
SOLANKI TRUSHEN <109092255+solankitrushen@users.noreply.github.com>
2024-10-09 22:07:07 +0530

    Update page.tsx

--- src/app/admin/nft-marketplace/page.tsx
+++ src/app/admin/nft-marketplace/page.tsx
@@ -18,16 +18,46 @@ import NftCard from 'components/card/NftCard';
 import { LikedNftsProvider } from 'components/card/LikedNftsContext';
 import Link from 'next/link';
 
+const nftData = [
+  {
+    bidders: [avatar1, avatar2, avatar3],
+    title: "Saturn Rings",
+    author: "Hubble",
+    price: "200 ETH",
+    image: NFt4,
+    extra: "Composed of ice particles",
+    onExplore: () => console.log('Exploring Saturn Rings'),
+  },
+  {
+    bidders: [avatar1, avatar2, avatar3],
+    title: "Jupiter's Storm",
+    author: "JPL",
+    price: "150 ETH",
+    image: NFt5,
+    extra: "Contains 300 ppm of carbon",
+    onExplore: () => console.log('Exploring Jupiter\'s Storm'),
+  },
+  {
+    bidders: [avatar1, avatar2, avatar3],
+    title: "Black Hole",
+    author: "NASA",
+    price: "300 ETH",
+    image: NFt6,
+    extra: "Has a mass of 10 solar masses",
+    onExplore: () => console.log('Exploring Black Hole'),
+  },
+];
+
 const Marketplace = () => {
   return (
     <LikedNftsProvider>
       <div className="overflow-hidden">
         <div className="mt-3 grid h-full grid-cols-1 gap-5 xl:grid-cols-2 2xl:grid-cols-3">
           <div className="col-span-1 h-fit w-full xl:col-span-1 2xl:col-span-2">
-            {/* NFt Banner */}
+            {/* NFT Banner */}
             <Banner />
 
-            {/* NFt Header */}
+            {/* NFT Header */}
             <div className="mb-4 mt-5 flex flex-col justify-between px-4 md:flex-row md:items-center">
               <h4 className="ml-1 text-2xl font-bold text-navy-700 dark:text-white">
                 <Link href="/admin/nft-marketplace">Space Discovery</Link>
@@ -41,7 +71,6 @@ const Marketplace = () => {
                     Galaxies
                   </Link>
                 </li>
-
                 <li>
                   <Link
                     href="/admin/nft-marketplace/Cards"
@@ -63,7 +92,7 @@ const Marketplace = () => {
                     href="/admin/nft-marketplace/Cards"
                     className="text-base font-medium text-brand-500 hover:text-brand-500 dark:text-white"
                   >
-                    Mones
+                    Moons
                   </Link>
                 </li>
               </ul>
@@ -71,39 +100,18 @@ const Marketplace = () => {
 
             {/* Space Discoveries trending card */}
             <div className="z-20 grid grid-cols-1 gap-5 md:grid-cols-3">
-        <NftCard
-  bidders={[avatar1, avatar2, avatar3]}
-  title="Saturn Rings"
-  author="Hubble"
-  price="200 ETH"  // Example price
-  image={NFt4}
-  extra="Composed of ice particles"  // Use extra instead of description
-  onExplore={() => console.log('Exploring Saturn Rings')}
-/>
-
-
-<NftCard
-  bidders={[avatar1, avatar2, avatar3]}
-  title="Saturn Rings"
-  author="Hubble"
-  price="200 ETH"  // Example price
-  image={NFt4}
-  extra="Composed of ice particles"  // Use extra instead of description
-  onExplore={() => console.log('Exploring Saturn Rings')}
-/>
-
-
-<NftCard
-  bidders={[avatar1, avatar2, avatar3]}
-  title="Saturn Rings"
-  author="Hubble"
-  price="200 ETH"  // Example price
-  image={NFt4}
-  extra="Composed of ice particles"  // Use extra instead of description
-  onExplore={() => console.log('Exploring Saturn Rings')}
-/>
-
-
+              {nftData.map((nft, index) => (
+                <NftCard
+                  key={index}
+                  bidders={nft.bidders}
+                  title={nft.title}
+                  author={nft.author}
+                  price={nft.price}
+                  image={nft.image}
+                  extra={nft.extra}
+                  onExplore={nft.onExplore}
+                />
+              ))}
             </div>
 
             {/* Recently Added section */}
@@ -115,41 +123,22 @@ const Marketplace = () => {
 
             {/* Recently Added Space Discoveries */}
             <div className="grid grid-cols-1 gap-5 md:grid-cols-3">
-              <NftCard
-  bidders={[avatar1, avatar2, avatar3]}
-  title="Saturn Rings"
-  author="Hubble"
-  price="200 ETH"  // Add the price prop if required
-  image={NFt4}
-  extra="Composed of ice particles"  // Use 'extra' instead of 'description'
-  onExplore={() => console.log('Exploring Saturn Rings')} 
-/>
-
-<NftCard
-  bidders={[avatar1, avatar2, avatar3]}
-  title="Jupiter's Storm"
-  author="JPL"
-  price="150 ETH"  // Add the price prop if required
-  image={NFt5}
-  extra="Contains 300 ppm of carbon"  // Use 'extra' instead of 'description'
-  onExplore={() => console.log('Exploring Jupiter's Storm')} 
-/>
-
-<NftCard
-  bidders={[avatar1, avatar2, avatar3]}
-  title="Black Hole"
-  author="NASA"
-  price="300 ETH"  // Add the price prop if required
-  image={NFt6}
-  extra="Has a mass of 10 solar masses"  // Use 'extra' instead of 'description'
-  onExplore={() => console.log('Exploring Black Hole')} 
-/>
-
+              {nftData.map((nft, index) => (
+                <NftCard
+                  key={index}
+                  bidders={nft.bidders}
+                  title={nft.title}
+                  author={nft.author}
+                  price={nft.price}
+                  image={nft.image}
+                  extra={nft.extra}
+                  onExplore={nft.onExplore}
+                />
+              ))}
             </div>
           </div>
 
-          {/* right side section */}
-
+          {/* Right side section */}
           <div className="col-span-1 h-full w-full rounded-xl 2xl:col-span-1">
             <TopCreatorTable tableData={tableDataTopCreators} />
             <div className="mb-5" />
@@ -162,36 +151,18 @@ const Marketplace = () => {
           </h4>
           <div className="flex w-full flex-col space-y-4 lg:flex-row lg:space-x-4 lg:space-y-0">
             <div className="flex-1">
-              <NftCard
-  bidders={[avatar1, avatar2, avatar3]}
-  title="Saturn Rings"
-  author="Hubble"
-  price="200 ETH"  // Add the price prop if required
-  image={NFt4}
-  extra="Composed of ice particles"  // Use 'extra' instead of 'description'
-  onExplore={() => console.log('Exploring Saturn Rings')} 
-/>
-
-<NftCard
-  bidders={[avatar1, avatar2, avatar3]}
-  title="Jupiter's Storm"
-  author="JPL"
-  price="150 ETH"  // Add the price prop if required
-  image={NFt5}
-  extra="Contains 300 ppm of carbon"  // Use 'extra' instead of 'description'
-  onExplore={() => console.log('Exploring Jupiter's Storm')} // 
-/>
-
-<NftCard
-  bidders={[avatar1, avatar2, avatar3]}
-  title="Black Hole"
-  author="NASA"
-  price="300 ETH"  // Add the price prop if required
-  image={NFt6}
-  extra="Has a mass of 10 solar masses"  // Use 'extra' instead of 'description'
-  onExplore={() => console.log('Exploring Black Hole')} 
-/>
-
+              {nftData.map((nft, index) => (
+                <NftCard
+                  key={index}
+                  bidders={nft.bidders}
+                  title={nft.title}
+                  author={nft.author}
+                  price={nft.price}
+                  image={nft.image}
+                  extra={nft.extra}
+                  onExplore={nft.onExplore}
+                />
+              ))}
             </div>
           </div>
         </div>
